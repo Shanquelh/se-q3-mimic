04b9ce68af3c508592775429efe2f1f7330931a5
SHANQUEL H. SCOTT <shanquelh@yahoo.com>
2020-08-21 17:08:28 -0500

    Passed all test for this assessment

--- mimic.py
+++ mimic.py
@@ -20,33 +20,26 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "???"
-
+__author__ = "Shanquel Scott doing in study group with Gabby and Sondos"
+____reference___ = "https://www.programmersought.com/article/74581508841/"
 
 import random
 import sys
 
 
 def create_mimic_dict(filename):
-    """Returns a dict mapping each word to a list of words which follow it.
-    For example:
-        Input: "I am a software developer, and I don't care who knows"
-        Output:
-            {
-                "" : ["I"],
-                "I" : ["am", "don't"],
-                "am": ["a"],
-                "a": ["software"],
-                "software" : ["developer,"],
-                "developer," : ["and"],
-                "and" : ["I"],
-                "don't" : ["care"],
-                "care" : ["who"],
-                "who" : ["knows"]
-            }
-    """
-    # +++your code here+++
-    pass
+    mimic_dict = dict()
+    with open(filename) as f:
+        words = f.read().split()
+        start_wording = ''
+        # Having a empty string will allow you to merge new list to wording
+        for word in words:
+            if start_wording not in mimic_dict:
+                mimic_dict[start_wording] = [word]
+            else:
+                mimic_dict[start_wording].append(word)
+            start_wording = word
+        return mimic_dict
 
 
 def print_mimic_random(mimic_dict, num_words):
@@ -58,8 +51,11 @@ def print_mimic_random(mimic_dict, num_words):
         - Randomly select a new word from the next-list
         - Repeat this process num_words times
     """
-    # +++your code here+++
-    pass
+    start_word = ''
+    for i in range(num_words + 1):
+        print(start_word, end=" ")
+        if start_word in mimic_dict.keys():
+            start_word = random.choice(mimic_dict.get(start_word))
 
 
 def main(args):
